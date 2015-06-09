20ee578f7124c0f61aa6fcfd07dd5233c7be3ab8
Stacy Lin <stayclin@gmail.com>
2015-06-08 23:41:12 -0400

    “adding”

--- etch.js
+++ etch.js
@@ -1,28 +1,24 @@
 $(document).ready(function(){
 	var $sketchpad = $('#sketchpad');
 
+	//set mode
+	var mode = "normal";
+
 	function createPad(padSize){
 		//draw a square of set size
 		//make the square size relative to sketchpad size
 		var padWidth, padHeight;
 		padWidth = $sketchpad.width();
 		padHeight = $sketchpad.height();
-		alert(padWidth + " " +padHeight);
-		//input from user
-		/*var numSquares, squareSize;
-		numSquares = 5;
-		squareSize = padWidth/numSquares;
-		//numSquares=padSize;
-		alert("squareSize " + squareSize);
-		*/
-		//var numSquares=10;
+		console.log(padWidth + " " +padHeight);
+
 		var numSquares = padSize;
 		var squareW = (padWidth / numSquares) + "px";
 		var squareH = (padHeight / numSquares) + "px";
 		//alert(squareH);
 		var totalSquares = numSquares*numSquares;
 		for(var i=0; i<totalSquares; i++){
-			$('<div class="square">').css('background-color', 'gray').appendTo($sketchpad);
+			$('<div class="square">').css('background-color', 'white').appendTo($sketchpad);
 			//borders take up extra space...
 			//$('<div class="square">').css('border', '1px solid black').appendTo($sketchpad);
 		}
@@ -30,31 +26,93 @@ $(document).ready(function(){
 		$($squares).css("width", squareW);
 		$($squares).css("height", squareH);
 		//alert("squareH " + squareH);
-		alert("squareSize " + ($squares).height());
+		console.log("squareSize " + ($squares).height());
 
 		//change color when hover over square
-		$($squares).hover(function(){
-      	$(this).css('background-color', 'white');
-   		});
+		squareHover();
+
 	}
 	createPad(16);
 
-		//change color when hover over square
-		//$($squares).hover(function(){
-      	//$(this).css('background-color', 'white');
-   		//});
+	//change color when hover over square
+	function squareHover(){
+		//if gray
+		if(mode == "normal"){
+			//$($squares).hover(function(){
+			//mouseover/mouseout, mouseenter/mouseleave
+			//hover accepts event for in and out, thats why changes color in and out
+			$($squares).mouseover(function(){
+				//simple one color 
+	      		//$(this).css('background-color', 'gray');
+
+				//console.log($(this).css('background-color'));
+				if($(this).css('background-color') === 'rgb(255, 255, 255)'){
+					console.log('testtt');
+	      			//$(this).css('background-color', 'gray');
+	      			$(this).css('background-color', 'black');
+	      			$(this).css('opacity', .25);
+				}
+				//subsequent hovers
+				//opacity returns a string
+				else{
+					console.log('more');
+					var newOpacity = parseFloat($(this).css('opacity')) + 0.25;
+					console.log(newOpacity);
+					$(this).css('opacity', newOpacity);
+				}
+      			//console.log("opacity: " + $(this).css('opacity'));
+   			});
+		}
+		//else random color hover
+		else if(mode == "random"){
+        	//alert('random color hover');
+        	$($squares).hover(function(){
+      			$(this).css('background-color', getRandomColor());
+   			});
+		}
+
+	}
+
 
     //button functions
+    //clears board
     $("#reset").click(function(){
         alert('reset button clicked');
         $sketchpad.empty();
         var newSize = prompt("Please enter a new size:");
         //alert(newSize);
         createPad(newSize);
-        //
-        //$($squares).hover(function(){
-    	//$(this).css('background-color', 'white');
-		//});
     });
+
+    $("#normal").click(function(){
+        //alert('normal button clicked');
+        $sketchpad.empty();
+        mode = "normal";
+        var newSize = prompt("Please enter a new size:");
+        //alert(newSize);
+        createPad(newSize);
+    });
+    
+    $("#random").click(function(){
+        //alert('random button clicked');
+        $sketchpad.empty();
+        mode = "random";
+        var newSize = prompt("Please enter a new size:");
+        //alert(newSize);
+        createPad(newSize);
+        //getRandomColor();
+    });
+
+    //get random color
+    function getRandomColor(){
+    	var letters = '0123456789ABCDEF'.split('');
+    	var color = '#';
+    	for (var i = 0; i < 6; i++ ) {
+        	color += letters[Math.floor(Math.random() * 16)];
+    	}
+    	//console.log(color);
+    	return color;
+    }
+
 
 });
